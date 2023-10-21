4dd2be82ebda30e49c67e9dfcbb50658006dfa61
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-10-21 19:41:17 +0300

    add artistList

---
+++ components/Artist/ArtistList.tsx
@@ -0,0 +1,56 @@
+import { IUserAuth } from "@/types/IAuth";
+import Image from "next/image";
+import styles from "@/styles/components/Artist/ArtistList.module.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const ArtistList = () => {
+  const [artistList, setArtistList] = useState<IUserAuth[]>([]);
+
+  useEffect(() => {
+    const fetchArtist = async () => {
+      try {
+        const response = await axios.get("/users/find");
+        setArtistList(response.data);
+      } catch (error) {
+        console.error("Ошибка при загрузке всех артистов:", error);
+      }
+    };
+
+    fetchArtist();
+    console.log(artistList);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <p>ArtistList</p>
+      <ul className={styles.artistList}>
+        {artistList &&
+          artistList.map((artist) => (
+            <li
+              key={artist._id}
+              className={styles.artistItem}>
+              <Image
+                src={artist.master_photo}
+                alt={"user photo"}
+                width={200}
+                height={200}
+              />
+              <div>
+                {" "}
+                <p> Назва: {artist.title}</p>
+                <p>Імя: {artist.firstName}</p>
+              </div>
+              <div>
+                {" "}
+                <p> Опис: {artist.description}</p>
+                <p>Ціна: {artist.price}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ArtistList;
--- pages/artists/index.tsx
+++ pages/artists/index.tsx
@@ -1,7 +1,13 @@
+import ArtistList from "@/components/Artist/ArtistList";
 import React from "react";
+import styles from "@/styles/Artist/Artist.module.css";
 
 const Artists = () => {
-  return <div>Artists</div>;
+  return (
+    <div className={styles.container}>
+      <ArtistList />
+    </div>
+  );
 };
 
 export default Artists;
