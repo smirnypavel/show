1e654283e516804b4b7fdef7e8c399aaed9797d0
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-11-19 23:44:55 +0200

    bag fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,9 @@
 // import RequestForm from "@/components/Home/RequestForm";
-import RequestForm2 from "@/components/RequestForm/RequestForm";
+
 import React from "react";
 
 const About = () => {
-  return (
-    <div>
-      About
-      <div>
-        <RequestForm2 />
-      </div>
-    </div>
-  );
+  return <div>About</div>;
 };
 
 export default About;
