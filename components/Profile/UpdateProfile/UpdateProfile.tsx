0e04b066ba805b317b5e57ee394aee4ce5edbfe8
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-12-23 23:02:01 +0200

    styles commit

--- components/Profile/UpdateProfile/UpdateProfile.tsx
+++ components/Profile/UpdateProfile/UpdateProfile.tsx
@@ -4,6 +4,7 @@ import Sidebar from "./Sidebar";
 import Component1 from "@/components/Profile/UpdateProfile/ProfileUpdateInfo"; // Подключи компоненты, которые тебе нужны
 import Component3 from "@/components/Profile/UpdateProfile/UploadMedia/UpdateMedia";
 import Component4 from "@/components/Profile/UpdateProfile/payment";
+import Component5 from "@/components/Profile/UpdateProfile/ChangePassword";
 import styles from "@/styles/components/Profile/UpdateProfile/UpdateProfile.module.css";
 import { IoIosArrowForward } from "react-icons/io";
 
@@ -33,6 +34,7 @@ const UpdateProfile: React.FC = () => {
           {activeComponent === "Component1" && <Component1 />}
           {activeComponent === "Component3" && <Component3 />}
           {activeComponent === "Component4" && <Component4 />}
+          {activeComponent === "Component5" && <Component5 />}
           {/* Добавь другие компоненты с помощью условий, если нужно */}
         </div>
       </div>
