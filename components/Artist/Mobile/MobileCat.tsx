d8e3b44a6bbfa6250a767c1beb8de0901a0dbb75
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2024-01-17 20:54:36 +0200

    mobile cat 1

--- components/Artist/Mobile/MobileCat.tsx
+++ components/Artist/Mobile/MobileCat.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/components/Artist/Mobile/MobileCat.module.css";
+import { IoIosArrowBack } from "react-icons/io";
 
 const MobileCat = () => {
+  const [isCatContainerVisible, setIsCatContainerVisible] = useState(false);
+
+  const toggleCatContainer = () => {
+    setIsCatContainerVisible(!isCatContainerVisible);
+  };
+
   return (
     <div>
-      <button className={styles.buttonCat}>Обрати категорію</button>
+      <button
+        className={`${styles.buttonCat} ${
+          isCatContainerVisible && styles.show
+        }`}
+        onClick={toggleCatContainer}>
+        Обрати категорію
+      </button>
+      <div
+        className={`${styles.catContainer} ${
+          isCatContainerVisible && styles.show
+        }`}>
+        <div className={styles.catContainerNav}>
+          <button className={styles.buttonNav}>
+            <IoIosArrowBack className={styles.buttonNavIcon} />
+          </button>
+          <button className={styles.buttonNav}>Скасувати</button>
+        </div>
+      </div>
     </div>
   );
 };
