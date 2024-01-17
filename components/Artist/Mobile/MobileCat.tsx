d26bcf0b14178a617b62b8ae1cb0b07e64d1b35d
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2024-01-17 22:16:00 +0200

    mobile cat

--- components/Artist/Mobile/MobileCat.tsx
+++ components/Artist/Mobile/MobileCat.tsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/components/Artist/Mobile/MobileCat.module.css";
 import { IoIosArrowBack } from "react-icons/io";
+import axios from "axios";
+import { ICategory } from "@/types/IAuth";
 
 const MobileCat = () => {
   const [isCatContainerVisible, setIsCatContainerVisible] = useState(false);
+  const [categories, setCategories] = useState<ICategory[]>([]);
 
   const toggleCatContainer = () => {
     setIsCatContainerVisible(!isCatContainerVisible);
   };
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get<ICategory[]>("/users/category");
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Ошибка при загрузке категорий:", error);
+        // Здесь можно добавить обработку ошибки, например, показать сообщение пользователю
+      }
+    };
+
+    fetchCategories();
+  }, []);
 
   return (
     <div>
@@ -27,6 +43,18 @@ const MobileCat = () => {
             <IoIosArrowBack className={styles.buttonNavIcon} />
           </button>
           <button className={styles.buttonNav}>Скасувати</button>
+        </div>
+        <div className={styles.catListContainer}>
+          <ul className={styles.catList}>
+            {categories.map((category) => (
+              <li
+                key={category._id}
+                className={styles.catListItem}>
+                <p className={styles.catListText}>{category.name}</p>
+                <IoIosArrowBack className={styles.catListIcon} />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
