df6cbb04e92978fe4594f0cc20a89564abcbd482
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2024-02-13 00:58:32 +0200

    some commit

--- components/Layout/LayoutProfile.tsx
+++ components/Layout/LayoutProfile.tsx
@@ -1,33 +1,85 @@
-import React from "react";
-import { PiUserLight, PiUserGearThin, PiUserListThin } from "react-icons/pi";
+import React, { useState } from "react";
+import {
+  PiUserLight,
+  PiUserGearThin,
+  PiUserListThin,
+  PiSkullThin,
+  PiSignOutThin,
+  PiKeyholeThin,
+  PiKeyThin,
+} from "react-icons/pi";
 import styles from "@/styles/Layout/LayoutProfile.module.css"; // импортируем файл со стилями
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Modal from "../helpers/Modal";
+import ChangePassword from "../Profile/UpdateProfile/ChangePassword";
 
 const LayoutProfile: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.nav}>
-        <Link
-          className={styles.navLink}
-          href={"/profile"}>
-          <PiUserLight className={styles.icon} /> Профіль
-        </Link>
-        <Link
-          className={styles.navLink}
-          href={"profile/update"}>
-          {" "}
-          <PiUserGearThin className={styles.icon} /> Налаштування
-        </Link>
-        <Link
-          className={styles.navLink}
-          href={"/profile/stats"}>
-          {" "}
-          <PiUserListThin className={styles.icon} /> Статистика
-        </Link>
+        <div className={styles.navLinksContainer}>
+          <Link
+            className={`${styles.navLink} ${
+              router.pathname === "/profile" ? styles.active : ""
+            }`}
+            href={"/profile"}>
+            <PiUserLight className={styles.icon} /> Профіль
+          </Link>
+          <Link
+            className={`${styles.navLink} ${
+              router.pathname === "/profile/update" ? styles.active : ""
+            }`}
+            href={"/profile/update"}>
+            <PiUserGearThin className={styles.icon} /> Налаштування
+          </Link>
+
+          <Link
+            className={`${styles.navLink} ${
+              router.pathname === "/profile/stats" ? styles.active : ""
+            }`}
+            href={"/profile/stats"}>
+            <PiUserListThin className={styles.icon} /> Статистика
+          </Link>
+        </div>
+        <div className={styles.service}>
+          <div className={styles.serviceLink}>
+            {" "}
+            <PiSignOutThin className={styles.serviceIcon} />
+            Вийти
+          </div>
+          <div
+            className={styles.serviceLink}
+            onClick={openModal}>
+            <PiKeyThin className={styles.serviceIcon} />
+            Змінити пароль
+          </div>
+          <div className={styles.serviceLink}>
+            {" "}
+            <PiSkullThin className={styles.serviceIcon} />
+            Видалити профіль
+          </div>
+        </div>
       </div>
       <div className={styles.content}>{children}</div>
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <ChangePassword />
+        </Modal>
+      )}
     </div>
   );
 };
