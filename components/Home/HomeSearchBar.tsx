1edeee9b8d4d25bcd4fbf1884443a033eb2e3745
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2024-03-01 05:20:06 +0200

    bag fix 2

--- components/Home/HomeSearchBar.tsx
+++ components/Home/HomeSearchBar.tsx
@@ -18,10 +18,10 @@ const HomeSearchBar = () => {
   useEffect(() => {
     const authenticateWithGoogle = async () => {
       try {
-        if (typeof token === "string" && !authenticated) {
+        if (typeof token === "string") {
           // Додано перевірку на authenticated
           const result = await dispatch(googleAuth(token));
-          if (!isReg && result.payload.success) {
+          if (!result.payload.data.register) {
             router.push("/profile/first-register");
             setAuthenticated(true);
           }
@@ -34,7 +34,7 @@ const HomeSearchBar = () => {
     if (token) {
       authenticateWithGoogle();
     }
-  }, [token, isReg]);
+  }, [token]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
