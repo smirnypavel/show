1dee7c7eca9eaadb881bd2075866ad7c50e27ed5
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2024-03-26 06:46:43 +0200

    bag fix commit

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ import FeedbackButton from "../helpers/Feedback";
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className={styles.wrapper}>
-      <ul className={styles.floatingBoxes}>
+      {/* <ul className={styles.floatingBoxes}>
         <li></li>
         <li></li>
         <li></li>
@@ -20,7 +20,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         <li></li>
         <li></li>
         <li></li>
-      </ul>
+      </ul> */}
       <Geolocation />
       <Header />
       <div className={styles.content}>{children}</div>
