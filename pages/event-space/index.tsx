2e37eb1f2209a1360962b0531e01a19c92fff49d
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2024-07-04 22:33:10 +0300

    slider

---
+++ pages/event-space/index.tsx
@@ -0,0 +1,12 @@
+import EventSpaceMain from "@/components/Event-space/EventSpaceMain";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <EventSpaceMain />
+    </div>
+  );
+};
+
+export default index;
