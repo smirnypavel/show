ffcd71ffd017d272f8aee48302b4320e2858aa76
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2024-07-31 18:59:28 +0300

    bag fix photo

--- components/Profile/UpdateProfile/UploadMedia/TestPhoto.tsx
+++ components/Profile/UpdateProfile/UploadMedia/TestPhoto.tsx
@@ -14,6 +14,7 @@ import { IPhoto } from "@/types/IAuth";
 import { RiLoader2Line } from "react-icons/ri";
 import styles from "@/styles/components/Profile/UpdateProfile/UpdateMedia/TestPhoto.module.css";
 
+// Вспомогательная функция для обработки загружаемых файлов
 const processFiles = (
   files: FileList | null,
   existingCount: number,
